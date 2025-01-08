254c6f5c557862211b3ec229896acfadf7744ba2
Choikyudong <slgmgm@naver.com>
2025-01-08 20:48:08 +0900

    refactor: 스웨거 설정변경
    테스트를 위해 스웨거 설정 수정함

--- src/main/java/com/kyulab/board/config/SwaggerConfig.java
+++ src/main/java/com/kyulab/board/config/SwaggerConfig.java
@@ -1,20 +1,37 @@
 package com.kyulab.board.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 
 @Configuration
 public class SwaggerConfig {
-	
+
 	@Bean
 	public OpenAPI apiDocDec() {
+		Info info = new Info()
+						.title("springdoc-openapi")
+						.version("0.0.2")
+						.description("여기는 board임");
+
+		SecurityRequirement securityRequirement = new SecurityRequirement().addList(HttpHeaders.AUTHORIZATION);
+		Components jwtTestComp = new Components() // 액세스 토큰인증 기능 추가
+				.addSecuritySchemes(HttpHeaders.AUTHORIZATION, new SecurityScheme()
+						.type(SecurityScheme.Type.HTTP)
+						.scheme("bearer")
+						.bearerFormat("JWT")
+						.in(SecurityScheme.In.HEADER)
+						.name(HttpHeaders.AUTHORIZATION));
+
 		return new OpenAPI()
-				.info(new Info()
-					.title("springdoc-openapi")
-					.version("0.0.1")
-					.description("여기는 board임"));
+				.info(info)
+				.components(jwtTestComp)
+				.addSecurityItem(securityRequirement);
 	}
-	
+
 }
